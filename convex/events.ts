c792b79a0c8f24b01d1d37a751edef1879d7417a
SAJED <sajed@gmail.com>
2025-04-14 22:38:37 +0300

    first commit

--- convex/events.ts
+++ convex/events.ts
@@ -69,16 +69,25 @@ export const createEvent = mutation({
   },
 });
 
+// export const getEvents = query({
+//   args: {},
+//   handler: async (ctx) => {
+//     return await ctx.db
+//       .query("events")
+//       .filter((q) => q.eq(q.field("iscancelled"), undefined))
+//       .collect();
+//   },
+// });
+ 
 export const getEvents = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db
       .query("events")
-      .filter((q) => q.eq(q.field("iscancelled"), undefined))
+      .filter((q) => q.lt(q.field("iscancelled"), null))
       .collect();
   },
 });
-
  
 export const getById = query({
   args: { eventId: v.id("events") },
