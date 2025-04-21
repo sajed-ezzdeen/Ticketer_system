b4046bd6c44a9fde5640deec10352042277afcc2
SAJED <sajed@gmail.com>
2025-04-21 20:05:24 +0300

    first commit

--- next.config.ts
+++ next.config.ts
@@ -3,18 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       hostname: "useful-perch-663.convex.cloud",
+  //       protocol: "https",
+  //     },
+  //     {
+  //       hostname: "befitting-caribou-927.convex.cloud", 
+  //       protocol: "https", 
+  //       pathname: "/api/storage/**",
+  //     }
+  //   ],
+  // }, 
+
   images: {
-    remotePatterns: [
-      {
-        hostname: "useful-perch-663.convex.cloud",
-        protocol: "https",
-      },
-      {
-        hostname: "befitting-caribou-927.convex.cloud", 
-        protocol: "https", 
-        pathname: "/api/storage/**",
-      }
-    ],
+    domains: ['befitting-caribou-927.convex.cloud', 'useful-perch-663.convex.cloud'],
   },
 };
 
